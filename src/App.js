b26e0926df4b7493fb5658bfff771e0ce1b2adbf
mgrinko <glukym@gmail.com>
2019-09-25 11:15:24 +0300

    Store was connected to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import store from './store';
 
 import { getTodos } from './api';
 import TodoList from './TodoList';
 
-const App = () => {
-  // //
-  // state = {
-  //   todos: []
-  // };
-  //
-  // const todos = this.state.todos;
-  //
-  // const setTodos = (newTodos) => {
-  //   this.setState({
-  //     todos: newTodos,
-  //   });
-  // };
-  const [todos, setTodos] = useState([]);
-
+const App = ({ qwe }) => {
   useEffect(() => {
     getTodos()
       .then(todos => {
@@ -30,13 +17,16 @@ const App = () => {
   }, []);
 
   const toggleTodo = (todoId) => {
-    const newTodos = todos.map(todo => {
+    const newTodos = qwe.map(todo => {
       return (todoId !== todo.id)
         ? todo
-        : { ...todo, completed: !todo.completed, };
+        : { ...todo, completed: !todo.completed };
     });
 
-    setTodos(newTodos);
+    store.dispatch({
+      type: 'SET_TODOS',
+      payload: newTodos,
+    });
   };
 
   return (
@@ -44,11 +34,30 @@ const App = () => {
       <h1>Todo APP with Redux</h1>
 
       <TodoList
-        todos={todos}
+        todos={qwe}
         toggleTodo={toggleTodo}
       />
     </main>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+  qwe: state.todos,
+});
+export default connect(mapStateToProps)(App);
+
+
+// //
+// const [todos, setTodos] = useState([]);
+// //
+// state = {
+//   todos: []
+// };
+//
+// const todos = this.state.todos;
+//
+// const setTodos = (newTodos) => {
+//   this.setState({
+//     todos: newTodos,
+//   });
+// };
