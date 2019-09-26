62a14823e90d29eaf3ec6dc1b8a79591693a86c5
tearfulmerlin <t380937709275@gmail.com>
2019-09-26 15:41:31 +0300

    toggleTodo action creator was implemented

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { toggleTodo } from './store';
 
-const TodoList = ({ todos, setTodos }) => {
-  const toggleTodo = (todoId) => {
-    const newTodos = todos.map(todo => {
-      return (todoId !== todo.id)
-        ? todo
-        : { ...todo, completed: !todo.completed };
-    });
-
-    setTodos(newTodos);
-  };
-
+const TodoList = ({ todos, setTodos, toggleTodoHandler }) => {
   return (
     <div className="TodoList">
       <strong>Todos:</strong>
@@ -24,7 +15,7 @@ const TodoList = ({ todos, setTodos }) => {
               <input
                 type="checkbox"
                 checked={todo.completed}
-                onChange={() => toggleTodo(todo.id)}
+                onChange={() => toggleTodoHandler(todo.id)}
               />
 
               {todo.title}
@@ -36,15 +27,16 @@ const TodoList = ({ todos, setTodos }) => {
   );
 };
 
-const mapState = (state) => ({
+const mapState = state => ({
   todos: state.todos,
 });
 
-const mapDispatch = (dispatch) => ({
-  setTodos: (todos) => dispatch({
+const mapDispatch = dispatch => ({
+  setTodos: todos => dispatch({
     type: 'SET_TODOS',
     payload: todos,
   }),
+  toggleTodoHandler: payload => dispatch(toggleTodo(payload)),
 });
 
 export default connect(mapState, mapDispatch)(TodoList);
@@ -56,5 +48,5 @@ export default connect(mapState, mapDispatch)(TodoList);
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
-  toggleTodo: PropTypes.func.isRequired
+  // toggleTodo: PropTypes.func.isRequired
 };
--- src/store.js
+++ src/store.js
@@ -1,5 +1,11 @@
 import { createStore } from 'redux';
 
+const TOGGLE_TODO = 'TOGGLE_TODO';
+
+export function toggleTodo(payload) {
+  return { type: TOGGLE_TODO, payload };
+}
+
 const initialState = {
   todos: [],
 };
@@ -11,6 +17,19 @@ const reducer = (state, action) => {
         ...state,
         todos: action.payload,
       };
+
+    case TOGGLE_TODO: {
+      const newTodos = state.todos.map(todo =>
+        (action.payload !== todo.id)
+          ? todo
+          : { ...todo, completed: !todo.completed }
+      );
+
+      return {
+        ...state,
+        todos: newTodos,
+      };
+    }
 
     default:
       return state;
