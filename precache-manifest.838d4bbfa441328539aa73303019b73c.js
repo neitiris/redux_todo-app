dc5f8359b69d7dc5cb4d3bc1db5853e6dd70d635
mgrinko <glukym@gmail.com>
2019-09-26 14:09:13 +0300

    Updates

---
+++ precache-manifest.838d4bbfa441328539aa73303019b73c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e63aa7623ab34ac1281e394528d8ea",
+    "url": "/redux_todo-app/index.html"
+  },
+  {
+    "revision": "9e4dfddc9e07e97e0f37",
+    "url": "/redux_todo-app/static/css/main.27eab42d.chunk.css"
+  },
+  {
+    "revision": "668dad4aee30b30b9914",
+    "url": "/redux_todo-app/static/js/2.3bd39f0f.chunk.js"
+  },
+  {
+    "revision": "9e4dfddc9e07e97e0f37",
+    "url": "/redux_todo-app/static/js/main.6a6e32b1.chunk.js"
+  },
+  {
+    "revision": "f6237b85156e86845fdb",
+    "url": "/redux_todo-app/static/js/runtime~main.dc666ae1.js"
+  }
+]);
