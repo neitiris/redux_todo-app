e8f3e8bedce7052b7126fe2237a637cb50267455
Nastia Krat <nastyakpi1995@gmail.com>
2019-09-26 15:10:06 +0300

    Solution

--- src/store.js
+++ src/store.js
@@ -1,30 +1,30 @@
-// import { createStore } from 'redux';
+import { createStore } from 'redux';
 
-// const initialState = {
-//   todos: [],
-// };
+const initialState = {
+  todos: [],
+};
 
-// const DELETE_TODO = 'DELETE_TODO';
-// export const deleteTodo = (value) => ({type: DELETE_TODO, value });
+const DELETE_TODO = 'DELETE_TODO';
 
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case 'SET_TODOS':
-//       return {
-//         ...state,
-//         todos: action.payload,
-//       };
-//       case DELETE_TODO:
-//         return {
-//           ...state,
-//           todos: state.todos.filter(todo => todo.id !== action.value),
-//         };
+export const deleteTodo = value => ({ type: DELETE_TODO, value });
 
-//     default:
-//       return state;
-//   }
-// };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_TODOS':
+      return {
+        ...state,
+        todos: action.payload,
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.value),
+      };
+    default:
+      return state;
+  }
+};
 
-// const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState);
 
-// export default store;
+export default store;
