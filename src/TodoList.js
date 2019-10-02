e7d7ba0641fff55602675c282925c0e07658ff11
Пользователь <Sviatoslv>
2019-10-02 12:10:21 +0300

    Renaming Implemented (with basic validation added)

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as todoActions from './redux/todos';
@@ -6,65 +6,124 @@ import * as todoActions from './redux/todos';
 const TodoList = ({
   todos,
   toggleTodo,
+  renameTodo,
   deleteTodo,
   placeFirst,
   moveUp,
   moveDown,
   placeLastTodo,
-}) => (
-  <div className="TodoList">
-    <strong>Todos:</strong>
+}) => {
+  const [newTitleOfTodo, setNewTitleOfTodo] = useState('');
+  const [editedTodoId, setEditedTodoId] = useState('');
 
-    <ul className="TodoList__list">
-      {todos.map(todo => (
-        <li key={todo.id} className="TodoList__item">
-          <label>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => toggleTodo(todo.id)}
-            />
-            {todo.title}
-          </label>
-          <button
-            type="button"
-            onClick={() => placeFirst(todo.id)}
+  const handleSaveNewTitle = (event, todoTitle) => {
+    event.preventDefault();
+
+    if (newTitleOfTodo && newTitleOfTodo !== todoTitle) {
+      renameTodo(editedTodoId, newTitleOfTodo);
+      setNewTitleOfTodo('');
+      setEditedTodoId('');
+    }
+  };
+
+  const handleNewTitleChange = (event, todoId) => {
+    setNewTitleOfTodo(event.target.value);
+    setEditedTodoId(todoId);
+  };
+
+  const handleEditedTodo = (todoId, todoTitle) => {
+    setNewTitleOfTodo(todoTitle);
+    setEditedTodoId(todoId);
+  };
+
+  const handleCancel = (todoTitle) => {
+    setNewTitleOfTodo(todoTitle);
+
+    if (todoTitle === newTitleOfTodo) {
+      setEditedTodoId('');
+    }
+  };
+
+  return (
+    <div className="TodoList">
+      <strong>Todos:</strong>
+
+      <ul className="TodoList__list">
+        {todos.map(todo => (
+          <li
+            key={todo.id}
+            className="TodoList__item"
+            onDoubleClick={() => handleEditedTodo(todo.id, todo.title)}
           >
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleTodo(todo.id)}
+              />
+              {todo.title}
+            </label>
+
+            <form
+              onSubmit={event => handleSaveNewTitle(event, todo.title)}
+              className={todo.id === editedTodoId ? '' : 'hide'}
+            >
+              <input
+                type="text"
+                value={todo.id === editedTodoId ? newTitleOfTodo : todo.title}
+                onChange={event => handleNewTitleChange(event, todo.id)}
+              />
+
+              <button type="submit">Save</button>
+
+              <button
+                type="button"
+                onClick={() => handleCancel(todo.title)}
+              >
+                Cancel
+              </button>
+            </form>
+
+            <button
+              type="button"
+              onClick={() => placeFirst(todo.id)}
+            >
             Place First
-          </button>
+            </button>
 
-          <button
-            type="button"
-            onClick={() => moveUp(todo)}
-          >
+            <button
+              type="button"
+              onClick={() => moveUp(todo)}
+            >
             Move up
-          </button>
+            </button>
 
-          <button
-            type="button"
-            onClick={() => moveDown(todo.id)}
-          >
+            <button
+              type="button"
+              onClick={() => moveDown(todo.id)}
+            >
             Move down
-          </button>
+            </button>
 
-          <button
-            type="button"
-            onClick={() => placeLastTodo(todo.id)}
-          >
+            <button
+              type="button"
+              onClick={() => placeLastTodo(todo.id)}
+            >
             Move to the end
-          </button>
+            </button>
 
-          <button
-            type="button"
-            onClick={() => deleteTodo(todo.id)}
-          >
+            <button
+              type="button"
+              onClick={() => deleteTodo(todo.id)}
+            >
             x
-          </button>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapState = state => ({
   todos: state.todos,
@@ -73,6 +132,9 @@ const mapState = state => ({
 const mapDispatch = dispatch => ({
   deleteTodo: todoId => dispatch(todoActions.deleteTodo(todoId)),
   toggleTodo: todoId => dispatch(todoActions.toggleTodo(todoId)),
+  renameTodo: (todoId, newTitle) => (
+    dispatch(todoActions.renameTodo(todoId, newTitle))
+  ),
   placeFirst: todoId => dispatch(todoActions.placeFirst(todoId)),
   moveUp: todo => dispatch(todoActions.moveUp(todo)),
   moveDown: todoId => dispatch(todoActions.moveDown(todoId)),
@@ -84,6 +146,7 @@ export default connect(mapState, mapDispatch)(TodoList);
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
   toggleTodo: PropTypes.func.isRequired,
+  renameTodo: PropTypes.func.isRequired,
   moveUp: PropTypes.func.isRequired,
   deleteTodo: PropTypes.func.isRequired,
   moveDown: PropTypes.func.isRequired,
