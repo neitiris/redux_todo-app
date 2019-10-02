80f14c48ca803dd2306c119229d6d34fa5b6ea8f
Nastia Krat <nastyakpi1995@gmail.com>
2019-10-02 10:56:26 +0300

    Solution

--- src/store.js
+++ src/store.js
@@ -17,6 +17,8 @@ const store = createStore(rootReducer, composeWithDevTools(
 ));
 
 export const getIsLoading = state => state.isLoading;
+export const getActiveTodos = state => state.todos
+  .filter(todo => todo.completed);
 export const getCompletedTodos = state => state.todos
   .filter(todo => todo.completed === true);
 
