9ef259be1d10e7431b4ba0c6246f257d669b311f
alexandr <alexandrov6927@gmail.com>
2019-10-04 12:09:53 +0300

    'higliting_selected_added'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import * as todoActions from './redux/todos';
 import * as loadingAction from './redux/loading';
 import * as selectors from './store';
 import TodoList from './TodoList';
-import TodosFilter from './todosFilter';
+import TodosFilter from './TodosFilter';
 
 const App = ({
   activeTodos,
--- src/todosFilter.js
+++ src/todosFilter.js
@@ -3,13 +3,40 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as filterAction from './redux/filter';
 
-const TodosFilter = ({ showAll, showCompleted, showActive }) => (
-  <div>
-    <button type="button" onClick={showAll}>All</button>
-    <button type="button" onClick={showCompleted}>Comleted</button>
-    <button type="button" onClick={showActive}>Active</button>
-  </div>
+const TodosFilter = ({
+  showAll,
+  showCompleted,
+  showActive,
+  filter,
+}) => (
+  <>
+    <button
+      type="button"
+      className={filter === 'ALL' && 'button_selected'}
+      onClick={showAll}
+    >
+    All
+    </button>
+    <button
+      type="button"
+      className={filter === 'COMPLETED' && 'button_selected'}
+      onClick={showCompleted}
+    >
+    Comleted
+    </button>
+    <button
+      type="button"
+      className={filter === 'ACTIVE' && 'button_selected'}
+      onClick={showActive}
+    >
+    Active
+    </button>
+  </>
 );
+
+const mapStateToProps = state => ({
+  filter: state.filter,
+});
 
 const mapDispatchToProps = dispatch => ({
   showAll: () => dispatch(filterAction.showAll()),
@@ -17,10 +44,11 @@ const mapDispatchToProps = dispatch => ({
   showActive: () => dispatch(filterAction.showActive()),
 });
 
-export default connect(null, mapDispatchToProps)(TodosFilter);
+export default connect(mapStateToProps, mapDispatchToProps)(TodosFilter);
 
 TodosFilter.propTypes = {
   showAll: PropTypes.func.isRequired,
   showCompleted: PropTypes.func.isRequired,
   showActive: PropTypes.func.isRequired,
+  filter: PropTypes.bool.isRequired,
 };
